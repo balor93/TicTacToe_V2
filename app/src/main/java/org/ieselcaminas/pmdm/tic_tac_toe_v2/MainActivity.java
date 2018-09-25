f6371539fb16a9f3ce26ad7d712ed1dcb129206b
jonatan <jonycs93@gmail.com>
2018-09-25 17:43:01 +0200

    Avoid overwritte buttons

--- app/src/main/java/org/ieselcaminas/pmdm/tic_tac_toe_v2/MainActivity.java
+++ app/src/main/java/org/ieselcaminas/pmdm/tic_tac_toe_v2/MainActivity.java
@@ -74,9 +74,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Button button = (Button) v;
-                button.setText(currentPlayer.symbol);
-                changePlayer();
-                displayTurn();
+
+                if(button.getText().equals("")) {
+                    button.setText(currentPlayer.symbol);
+                    changePlayer();
+                    displayTurn();
+                }
+
             }
         });
     }
